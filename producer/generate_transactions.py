4cdec8db0a91159998424a03bdd2af6174ef847d
bakliwalvaibhav1 <vbakliwalcodes@gmail.com>
2025-04-12 04:36:38 -0500

    feat: add real-time fake transaction generator using Faker

---
+++ producer/generate_transactions.py
@@ -0,0 +1,23 @@
+from faker import Faker
+import uuid
+import time
+import random
+from datetime import datetime
+
+fake = Faker()
+
+def generate_transaction():
+    return {
+        "transaction_id": str(uuid.uuid4()),
+        "user_id": f"user_{random.randint(1, 1000)}",
+        "timestamp": datetime.utcnow().isoformat(),
+        "amount": round(random.uniform(1.00, 1000.00), 2),
+        "location": fake.city() + ", " + fake.country(),
+        "merchant": fake.company(),
+        "card_type": random.choice(["VISA", "MASTERCARD", "AMEX"])
+    }
+
+while True:
+    txn = generate_transaction()
+    print(txn)
+    time.sleep(1)
